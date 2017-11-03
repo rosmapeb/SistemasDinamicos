024c9a5178ee58547cf6a3813a014113d54b7690
rosmapeb <rosmapeb@gmail.com>
2017-11-03 15:18:55 -0600

    modificacion1

--- fibonacci.py
+++ fibonacci.py
@@ -1,5 +1,23 @@
 import numpy as np
 
 Fk = 0
-F1 = 1
+Fk1 = 1
+fibonacci = [Fk]
+Fkn = 0
 
+# FORMULA ES Fk+2 = Fk+1 + Fk
+
+for fib in range(0,1):
+    print ('Fibonacci', fibonacci)
+    print ('Fib', fib)
+    print ('Fkn', Fkn)
+    if Fkn <= 1:
+        Fkn = fib + Fk1
+        Fk1 = Fkn
+        print (Fkn)
+        fibonacci.append(Fkn)
+    #else:
+     #   Fk1 = Fkn
+     #   Fkn = Fkn + Fk1
+     #   fibonacci.append(Fkn)
+     #   print (Fkn)
